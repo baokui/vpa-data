fa0aa111f80f01929f24f005844d75fad37dc7d2
apple-guobk <baokui2011@163.COM>
2021-03-10 10:04:00 +0800

    朋友圈点击数据

--- allScene_new/run_main.sh
+++ allScene_new/run_main.sh
@@ -9,5 +9,5 @@ Year=2021
 Month=03
 for((Day=1;Day<10;Day++))
 do
-sh main0.sh $Year $Month $Day >> log/main0-$Year$Month0$Day.log 2>&1 &
+sh main0.sh $Year $Month 0$Day >> log/main0-$Year$Month0$Day.log 2>&1 &
 done
